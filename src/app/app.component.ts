0dd2cb89deb2f54c8f36e7aa8f7facc086ee763f
bibhu-padhy <bibhupadhy140@gmail.com>
2023-11-30 16:59:11 +0530

    game board service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,69 +6,33 @@ import { BoardNumber, GameBoardService } from './game-board.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  boardNumbers: BoardNumber[] = []
+  boardNumbers: number[][][] = [];
   tickets: PlayerTicket = [];
   currentNumber: number | null = null;
   countdown = 30; // Countdown from 30 seconds
   private subscription: Subscription | undefined;
 
-  constructor(private randomNumberService: RandomNumberService,
+  constructor(
+    private randomNumberService: RandomNumberService,
     private ticketService: TicketService,
-    private gameBoardService: GameBoardService) { }
-
+    private gameBoardService: GameBoardService
+  ) {}
 
   ngOnInit() {
-
     // this.subscription = interval(1000).subscribe(() => {
     //   if (this.countdown > 0) {
     //     this.countdown--;
     //   } else {
-    //     this.countdown = 30; // Reset countdown
+    //     this.countdown = 10; // Reset countdown
     //     this.currentNumber = this.randomNumberService.getNextNumber();
     //   }
     // });
-
-
-    let ONE_TO_NINE = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-    for (let i = 1; i < 10; i++) {
-      ONE_TO_NINE.push(i)
-    }
-    ONE_TO_NINE = this.shuffleArray(ONE_TO_NINE)
-    console.log(ONE_TO_NINE);
+    this.boardNumbers = this.gameBoardService.generateBoard();
+    console.log(this.gameBoardService.generateBoard());
   }
-
-  shuffleArray(array: number[]): number[] {
-    let n = array.length;
-
-    // Initialize the zero count
-    let zeroCount = 0;
-
-    for (let i = n - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-
-      // Check for consecutive zeros condition
-      if (array[i] === 0 || array[j] === 0) {
-        if (zeroCount >= 3) {
-          // If there are already 3 consecutive zeros, skip this iteration
-          continue;
-        }
-        zeroCount++;
-      } else {
-        // Reset zero count if current elements are not zeros
-        zeroCount = 0;
-      }
-
-      // Swap elements
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-
-    return array;
-  }
-
 
   ngOnDestroy() {
     this.subscription?.unsubscribe();
--- src/app/game-board.service.ts
+++ src/app/game-board.service.ts
@@ -7,33 +7,95 @@ export interface BoardNumber {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameBoardService {
-  private boardNumbers: BoardNumber[];
-  private calledNumbers: Set<number>;
+  generateBoard(): number[][][] {
+    const ticketsCount = 6;
+    const rowsPerTicket = 3;
+    const colsPerTicket = 9;
+    const numbersPerRow = 5;
+    const numbersPerTicket = 15;
+    const boardNumbers: number[][][] = [];
 
-  constructor() {
-    this.boardNumbers = Array.from({ length: 90 }, (_, i) => ({ number: i + 1, called: false }));
-    this.calledNumbers = new Set();
+    // Initialize the board with empty tickets
+    for (let ticketIndex = 0; ticketIndex < ticketsCount; ticketIndex++) {
+      let ticket = Array.from({ length: rowsPerTicket }, () =>
+        new Array(colsPerTicket).fill(0)
+      );
+      boardNumbers.push(ticket);
+    }
+
+    // Define the number ranges for each column
+    const ranges = [
+      { start: 1, end: 9 },
+      { start: 10, end: 19 },
+      { start: 20, end: 29 },
+      { start: 30, end: 39 },
+      { start: 40, end: 49 },
+      { start: 50, end: 59 },
+      { start: 60, end: 69 },
+      { start: 70, end: 79 },
+      { start: 80, end: 90 },
+    ];
+
+    // Fill the board with actual numbers
+    ranges.forEach((range, colIndex) => {
+      let columnNumbers = this.shuffleArray(this.range(range.start, range.end));
+      let placeholderCount = Math.floor(
+        (ticketsCount * numbersPerRow) / rowsPerTicket
+      );
+      columnNumbers = columnNumbers.slice(0, placeholderCount); // Only take as many numbers as placeholders
+
+      // Distribute column numbers across all tickets
+      columnNumbers.forEach((number) => {
+        let placed = false;
+        while (!placed) {
+          let ticketIndex = this.getRandomInt(0, ticketsCount - 1);
+          let rowIndex = this.getRandomInt(0, rowsPerTicket - 1);
+          if (boardNumbers[ticketIndex][rowIndex][colIndex] === 0) {
+            boardNumbers[ticketIndex][rowIndex][colIndex] = number;
+            placed = true;
+          }
+        }
+      });
+    });
+
+    // Replace any undefined values with 0
+    boardNumbers.forEach((ticket) => {
+      ticket.forEach((row) => {
+        for (let i = 0; i < row.length; i++) {
+          if (row[i] === undefined) {
+            row[i] = 0;
+          }
+        }
+      });
+    });
+
+    return boardNumbers;
   }
 
-  private generateRandomNumber(): number {
-    let randomNum;
-    do {
-      randomNum = Math.floor(Math.random() * 90) + 1;
-    } while (this.calledNumbers.has(randomNum));
-    return randomNum;
+  // The getRandomInt, range, and shuffleArray functions remain unchanged
+
+  private getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  public callNumber(): number {
-    const numberToCall = this.generateRandomNumber();
-    this.boardNumbers[numberToCall - 1].called = true;
-    this.calledNumbers.add(numberToCall);
-    return numberToCall;
+  // The range and shuffleArray functions remain unchanged
+
+  // The range function remains unchanged
+
+  private range(start: number, end: number): number[] {
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
   }
 
-  public getBoardNumbers(): BoardNumber[] {
-    return this.boardNumbers;
+  private shuffleArray(array: number[]): number[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 }
