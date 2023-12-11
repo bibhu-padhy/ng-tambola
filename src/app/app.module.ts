8f958d894df0770812bd3693341bc978de23498f
Bibhu Padhy <bppadhi@winsupplyinc.com>
2023-12-11 17:41:21 +0530

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId":
+        "tambola-xyz", "appId": "1:628428357020:web:3d4601c12c4893231d1c59",
+      "databaseURL": "https://tambola-xyz-default-rtdb.firebaseio.com",
+      "storageBucket": "tambola-xyz.appspot.com",
+      "apiKey": "AIzaSyDgh5K-HHBXK6zBIxRvJZEt4SmOTKtux54",
+      "authDomain": "tambola-xyz.firebaseapp.com",
+      "messagingSenderId": "628428357020",
+      "measurementId": "G-QBEGQK1517"
+    })),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
